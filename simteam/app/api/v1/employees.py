06d0ee2331ebba31d4bb49a28e162e42d7ce8f1a
Tien Dung Pham <tiendung.pham@csl.com.au>
2025-07-20 16:38:45 +1000

    FastAPI works

--- simteam/app/api/v1/employees.py
+++ simteam/app/api/v1/employees.py
@@ -1,8 +1,8 @@
 # app/api/v1/employees.py
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.db.session import get_db
-from app.db.models import EmployeeORM
+from simteam.app.db.session import get_db
+from simteam.app.db.models import EmployeeORM
 from simteam.core.models.base import EmployeeState
 
 router = APIRouter()
